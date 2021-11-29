6947c4782054c360a0fe2cc1e3743cd882024c6a
NFoithong <n.foithong1983@gmail.com>
2021-11-29 01:57:35 -0500

    upto 3.1.5

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,25 @@
-window.alert("This is an alert! Javascript is running!");
+// This creates a function named "fight"
+// var playerName = window.prompt("What is your robot's name?")
+
+// // window.alert(playerName);
+// console.log(playerName);
+
+// console.log("This logs a string, good for leaving yourself a message");
+// // This will do math and log 20
+// console.log(10 + 10);
+// // What is this?
+// console.log("Our robot's name is " + playerName);
+// function fight() {
+//     window.alert("The fight has begun!");
+// }
+
+var playerName = "Tony the Robot";
+
+// Tony the Robot is ready for battle!
+console.log("Tony the Robot" + " is ready for battle!");
+
+// "Tony the Robot is ready for battle!"
+console.log(playerName + " is ready for battle!");
+
+// "Your robot, Tony the Robot, has won!
+console.log("Your robot, " + playerName + ", has won!");
