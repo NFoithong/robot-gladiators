4147104cd84bf40cb2f8f18bdba4aaab4aab244f
NFoithong <n.foithong1983@gmail.com>
2021-11-29 23:04:43 -0500

    update README

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # robot-gladiators
 JavaScript to code a Robot Gladiators.
+
+## This creates a function named "fight"
+
+```js
+var playerName = window.prompt("What is your robot's name?")
+    window.alert(playerName);
+console.log(playerName);
+```
+```js
+// This will do math and log 20
+console.log(10 + 10);
+```
+
+```js
+var playerName = "Tony the Robot";
+
+// Tony the Robot is ready for battle!
+console.log("Tony the Robot" + " is ready for battle!");
+
+// "Tony the Robot is ready for battle!"
+console.log(playerName + " is ready for battle!");
+
+// "Your robot, Tony the Robot, has won!
+console.log("Your robot, " + playerName + ", has won!");
+```
+
+## Create function
+
+```js
+// Function expression - create function
+var fight = function() {
+    alert("Welcome " + enemyName + " to Robot Gladiators!");
+};
+// execute function
+fight();
+
+// FUnction declaration - create function
+function fight(){
+    alert("Welcome " + enemyName + " to Robot Gladiators!");
+};
+// execute function
+fight();
+```
+
+## Checked condition 
+
+```js
+var health = 10;
+
+if (health < 0) {
+    console.log("This is not runnging!");
+} else {
+    console.log("This run instead!");
+}
+```
--- assets/js/game.js
+++ assets/js/game.js
@@ -1,45 +1,3 @@
-// This creates a function named "fight"
-// var playerName = window.prompt("What is your robot's name?")
-
-// // window.alert(playerName);
-// console.log(playerName);
-
-// console.log("This logs a string, good for leaving yourself a message");
-// // This will do math and log 20
-// console.log(10 + 10);
-// // What is this?
-// console.log("Our robot's name is " + playerName);
-// function fight() {
-//     window.alert("The fight has begun!");
-// }
-
-// var playerName = "Tony the Robot";
-
-// // Tony the Robot is ready for battle!
-// console.log("Tony the Robot" + " is ready for battle!");
-
-// // "Tony the Robot is ready for battle!"
-// console.log(playerName + " is ready for battle!");
-
-// // "Your robot, Tony the Robot, has won!
-// console.log("Your robot, " + playerName + ", has won!");
-
-
-// // Function expression - create function
-// var fight = function() {
-//     alert("Welcome " + enemyName + " to Robot Gladiators!");
-// };
-// // execute function
-// fight();
-
-// // FUnction declaration - create function
-// function fight(){
-//     alert("Welcome " + enemyName + " to Robot Gladiators!");
-// };
-// // execute function
-// fight();
-
-
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 20;
@@ -79,14 +37,3 @@ var fight = function() {
 };
 
 fight();
-
-
-
-// //  checked condition 
-// var health = 10;
-
-// if (health < 0) {
-//     console.log("This is not runnging!");
-// } else {
-//     console.log("This run instead!");
-// }
