8e45ab98b09637f5634e2281eb4bd1f4bbefcda3
NFoithong <n.foithong1983@gmail.com>
2021-12-01 01:03:40 -0500

    fix bug/playername

--- assets/js/game.js
+++ assets/js/game.js
@@ -206,12 +206,25 @@ var shop = function() {
 
 /* END GAME FUNCTIONS */
 
+// function to set name
+var getPlayerName = function() {
+    var name = "";
+
+    // ***************************************
+    while (name === "" || name === null) {
+        name = prompt("What is your robot's name?");
+    } // ***************************************
+
+    console.log("Your robot's name is " + name);
+    return name;
+};
+
 /* GAME INFORMATION / VARIABLES */
 
 //function to generate a random numeric value
 
 var playerInfo = {
-    name: prompt("What is your robot's name"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
